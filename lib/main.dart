7a1734775b5a2e026a18da43f3d27097201e1e9a
Saba <sabajaved524@gmail.com>
2023-06-07 20:41:35 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.red,
+      appBar: AppBar(
+        title: Text("Dice"),
+        backgroundColor: Colors.red,
+      ),
+      body: DicePage(),
+    ),
+  ));
+}
+
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  var leftDiceNumber = 1;
+  var rightDiceNumber = 1;
+
+  void diceeFunc() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+              child: TextButton(
+            onPressed: () {
+              diceeFunc();
+            },
+            child: Image.asset('images/dice$leftDiceNumber.png'),
+          )),
+          Expanded(
+              child: TextButton(
+                  onPressed: () {
+                    diceeFunc();
+                  },
+                  child: Image.asset('images/dice$rightDiceNumber.png'))),
+        ],
+      ),
+    );
+  }
+}
